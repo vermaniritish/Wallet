cdab0c9cabf89280927fd3fe29909f2afa5f52ea
Ritish <devteam@yazoeat.com.au>
2025-10-07 07:13:51 +0530

    db

--- rakesh.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `admins`
-ADD `shop_id` varchar(255) COLLATE 'utf8_general_ci' NULL AFTER `id`,
-CHANGE `modified` `modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP AFTER `address`;
