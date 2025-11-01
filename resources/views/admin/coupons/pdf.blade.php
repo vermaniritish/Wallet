3aa90689cb721a8abb945f11e666acb0634095b1
Ritish <devteam@yazoeat.com.au>
2025-11-01 13:51:24 +0530

    Update pdf.blade.php

--- resources/views/admin/coupons/pdf.blade.php
+++ resources/views/admin/coupons/pdf.blade.php
@@ -15,9 +15,9 @@
         <div style="max-width: 800px; margin: auto; padding: 30px; border: 1px solid #eee;line-height: 20px; font-family: Arial, sans-serif; color: #2F3751;">
             <table width="650" border="0" cellspacing="0" cellpadding="0">
             <tr>
-                <td colspan="2"><div align="center">
+                <td colspan="2" style="padding-top: 20px;padding-bottom: 30px"><div align="center">
                     @if($logo)
-                    <img src="<?php echo public_path($logo) ?>" />
+                    <img src="<?php echo public_path($logo) ?>" style="height: 60px;" />
                     @endif
                 </div></td>
             </tr>
@@ -26,7 +26,7 @@
                 
                 <p>Please find below your unique voucher code for use at&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <br /><br/>
                 <a href="https://pindersschoolwear.com/" target="_blank">www.pindersschoolwear.com</a> </p><br/>
-                <p>This is a <strong>{{$page->title}}</strong> discount code for the amount of {{$page->is_percentage ? ($page->amount . '%') : '&pound;'.$page->amount }}.</p><br/>
+                <p>This is a <strong>{{$page->title}}</strong> discount voucher for the amount of {{$page->is_percentage ? ($page->amount . '%') : '£'.$page->amount }}.</p><br/>
                 <p>Please ensure you use the full amount as the code will not be valid for any future purchases.</p><br/>
                 <p>This code will expire on {{_d($page->end_date)}}.</p><br/>
                 <p>Unique Voucher Code:</p><br/>
