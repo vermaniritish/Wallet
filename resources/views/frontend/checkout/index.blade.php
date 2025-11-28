a8dd376de320b80c50e85b85468e07c9d127ab09
Ritish <devteam@yazoeat.com.au>
2025-11-28 20:58:10 +0530

    cart empty

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -210,8 +210,8 @@ const initPaypal = function()
                 }).then(res => res.json())
                 .then(details => {
                     if(details?.status && details?.id) {
-                        localStorage.setItem('cart', "");
-                        localStorage.setItem('coupon', "");
+                        localStorage.removeItem('cart');
+                        localStorage.removeItem('coupon');
                         window.location.href = site_url + "/paypal/success?id=" + details.id;
                     } else {
                         window.location.href = site_url + "/paypal/error";
