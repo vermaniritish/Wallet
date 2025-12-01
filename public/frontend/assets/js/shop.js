f7436cf62f27d4d268817cb75c86473d3fb727d6
Ritish <devteam@yazoeat.com.au>
2025-12-01 22:43:35 +0530

    Update shop.js

--- public/frontend/assets/js/shop.js
+++ public/frontend/assets/js/shop.js
@@ -1,83 +1,79 @@
-(function ($) {
-    'use strict';
-    /*Product Details*/
-    var productDetails = function () {
-        $('.product-image-slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: false,
-            asNavFor: '.slider-nav-thumbnails',
-            
-        });
-
-        $('.slider-nav-thumbnails').slick({
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            asNavFor: '.product-image-slider',
-            dots: false,
-            focusOnSelect: true,
-            prevArrow: '<button type="button" class="slick-prev"><i class="fi-rs-angle-left"></i></button>',
-            nextArrow: '<button type="button" class="slick-next"><i class="fi-rs-angle-right"></i></button>'
-        });
-
-        // Remove active class from all thumbnail slides
-        $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
-
-        // Set active class to first thumbnail slides
-        $('.slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
-
-        // On before slide change match active thumbnail to current slide
-        $('.product-image-slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-            var mySlideNumber = nextSlide;
-            $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
-            $('.slider-nav-thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
-        });
-
-        $('.product-image-slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-            var img = $(slick.$slides[nextSlide]).find("img");
-            $('.zoomWindowContainer,.zoomContainer').remove();
-            $(img).elevateZoom({
-                zoomType: "inner",
-                cursor: "crosshair",
-                zoomWindowFadeIn: 500,
-                zoomWindowFadeOut: 750
-            });
-        });
-        //Elevate Zoom
+/*Product Details*/
+var productDetails = function () {
+    $('.product-image-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: false,
+        asNavFor: '.slider-nav-thumbnails',
         
-        //Filter color/Size
-
-        $('.dropdown-menu .cart_list').on('click', function (event) {
-            event.stopPropagation();
-        });
-    };
-
-    var initSlickZoom = function() {
-        if ( $(".product-image-slider").length ) {
-            $('.product-image-slider .slick-active img').elevateZoom({
-                zoomType: "inner",
-                cursor: "crosshair",
-                zoomWindowFadeIn: 500,
-                zoomWindowFadeOut: 750
-            });
-        }
-    }
-    var destroySlickZoom = function() {
-        var $img = $('.product-image-slider .slick-active img');
-        if ($img.data('elevateZoom')) {
-            $img.data('elevateZoom').destroy();   // destroy plugin
-            $img.removeData('elevateZoom');       // remove stored data
-            $('.zoomContainer').remove();         // remove zoom container from DOM
-        }
-    }
-    /* WOW active */
-    new WOW().init();
-
-    //Load functions
-    $(document).ready(function () {
-        productDetails();
-        initSlickZoom();
     });
 
-})(jQuery);
+    $('.slider-nav-thumbnails').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.product-image-slider',
+        dots: false,
+        focusOnSelect: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fi-rs-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fi-rs-angle-right"></i></button>'
+    });
+
+    // Remove active class from all thumbnail slides
+    $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+
+    // Set active class to first thumbnail slides
+    $('.slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
+
+    // On before slide change match active thumbnail to current slide
+    $('.product-image-slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        var mySlideNumber = nextSlide;
+        $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+        $('.slider-nav-thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
+    });
+
+    $('.product-image-slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        var img = $(slick.$slides[nextSlide]).find("img");
+        $('.zoomWindowContainer,.zoomContainer').remove();
+        $(img).elevateZoom({
+            zoomType: "inner",
+            cursor: "crosshair",
+            zoomWindowFadeIn: 500,
+            zoomWindowFadeOut: 750
+        });
+    });
+    //Elevate Zoom
+    
+    //Filter color/Size
+
+    $('.dropdown-menu .cart_list').on('click', function (event) {
+        event.stopPropagation();
+    });
+};
+
+var initSlickZoom = function() {
+    if ( $(".product-image-slider").length ) {
+        $('.product-image-slider .slick-active img').elevateZoom({
+            zoomType: "inner",
+            cursor: "crosshair",
+            zoomWindowFadeIn: 500,
+            zoomWindowFadeOut: 750
+        });
+    }
+}
+var destroySlickZoom = function() {
+    var $img = $('.product-image-slider .slick-active img');
+    if ($img.data('elevateZoom')) {
+        $img.data('elevateZoom').destroy();   // destroy plugin
+        $img.removeData('elevateZoom');       // remove stored data
+        $('.zoomContainer').remove();         // remove zoom container from DOM
+    }
+}
+/* WOW active */
+new WOW().init();
+
+//Load functions
+$(document).ready(function () {
+    productDetails();
+    initSlickZoom();
+});
