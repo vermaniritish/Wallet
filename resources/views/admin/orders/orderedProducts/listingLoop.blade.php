3c8adbf586929a9aac7f0e2fe0aafc34ba4d6cc5
Ritish <devteam@yazoeat.com.au>
2025-12-02 00:13:10 +0530

    Update listingLoop.blade.php

--- resources/views/admin/orders/orderedProducts/listingLoop.blade.php
+++ resources/views/admin/orders/orderedProducts/listingLoop.blade.php
@@ -23,42 +23,10 @@
 	</td>
 </tr>
 <?php 
-$logos = $row->logo_data ? (substr($row->logo_data, 0, 1) == '{' ? json_decode('['.$row->logo_data.']') : json_decode($row->logo_data)) : null; 
+pr($row->logo_data);
+$logos = $row->logo_data ? (substr($row->logo_data, 0, 1) == '{' ? json_decode('['.$row->logo_data.']') : json_decode($row->logo_data)) : []; 
+pr($logos); die;
 ?>
-<?php if($logos): ?>
-@foreach($logos as $logo)
-<?php if(!($logo->text || $logo->image || $logo->category || $logo->postion)) continue; ?>
-<tr class="table-borderless">
-	<td></td>
-	<td colspan="4">
-		<div class="row">
-			@if(isset($logo->already_uploaded) && $logo->already_uploaded)
-			<div class="col-sm-2">
-				<p class="text-danger">Pinder has already my logo.</p>
-			</div>
-			@else			
-			<div class="col-sm-2">
-				<div class="" style="width: 80px;height:80px;border: 1px solid #ddd;">
-					<?php if(trim($logo->image)):?>
-					<img src="{{ $logo->image }}" style="max-width:100%;max-height:100%;">
-					<?php endif; ?>
-				</div>
-			</div>
-			@endif
-			<div class="col-sm-5">
-				<span class="text-muted">Text:</span> <?php echo $logo->text ?><br />
-				<span class="text-muted">Category:</span> <?php echo $logo->category ?><br />
-				<span class="text-muted">Position:</span> <?php echo $logo->postion ?><br />
-			</div>
-			<div class="col-sm-4">
-				<span class="text-muted">Size:</span> <?php echo $row->size_title ?><br />
-				<span class="text-muted">Color:</span> <?php echo $row->color ?><br />
-			</div>
-		</div>
-	</td>
-</tr>
-@endforeach
-
 @foreach($logos as $logo)
 <?php if(!($logo->text || $logo->image || $logo->category || $logo->postion)) continue; ?>
 <tr class="table-borderless">
