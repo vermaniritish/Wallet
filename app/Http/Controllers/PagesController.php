36e818bfa5ea2c92a0171c5416cd73e4d52fd7a9
Ritish <devteam@yazoeat.com.au>
2025-12-07 12:34:34 +0530

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -341,8 +341,6 @@ class PagesController extends BaseController
     function personalization(Request $request)
     {
         $categories = ProductCategories::select(['id', 'slug', 'image', 'title'])->where('status', 1)->get();
-        pr($categories->toArray()); die;
-
         return view('frontend.personalization', [
             'categories' => $categories
         ]);
