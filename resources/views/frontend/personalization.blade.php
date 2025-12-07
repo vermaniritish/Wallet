9a9542be40e1c944043f9a0426592730eb55287d
Ritish <devteam@yazoeat.com.au>
2025-12-07 12:36:28 +0530

    Update personalization.blade.php

--- resources/views/frontend/personalization.blade.php
+++ resources/views/frontend/personalization.blade.php
@@ -21,14 +21,7 @@
                     <div class="carausel-6-columns-cover position-relative">
                         <div class="slider-arrow slider-arrow-2 carausel-6-columns-arrow" id="carausel-6-columns-arrows"></div>
                         <div class="carausel-6-columns" id="carausel-6-columns">
-                            @foreach($categories as $c)
-                            <div class="card-1">
-                                <figure class="img-hover-scale overflow-hidden">
-                                    
-                                </figure>
-                                <h5><a href="{{ $c->slug ? '/'.$c->slug : ''}}">{{$c->title}}</a></h5>
-                            </div>
-                            @endforeach
+                            
                         </div>
                     </div>
                 </div>
