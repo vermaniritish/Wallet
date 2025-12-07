aeb6b5dde46b8249dc9ede52fc46a4430ccb356c
Ritish <devteam@yazoeat.com.au>
2025-12-07 12:36:58 +0530

    Update personalization.blade.php

--- resources/views/frontend/personalization.blade.php
+++ resources/views/frontend/personalization.blade.php
@@ -1,32 +1,4 @@
 @extends('layouts.frontendlayout')
 @section('content')
-<div class="page-header breadcrumb-wrap">
-    <div class="container">
-        <div class="breadcrumb">
-            <a href="{{url('/)}}" rel="nofollow">Home</a>
-            <span></span> Shop
-            <span></span> Customize Your Apparel
-        </div>
-    </div>
-</div>
-<section class="mt-50 mb-50" id="cart-page">
-    <div class="container">
-        <div class="row g-4">
-            <div class="col-lg-12">
-                <img src="personalise.jpg" alt="Personalise Your Apparel" />
-            </div>
-            <section class="popular-categories section-padding mt-15">
-                <div class="container wow fadeIn animated">
-                    <h3 class="section-title mb-20"><span>Select</span> Categories</h3>
-                    <div class="carausel-6-columns-cover position-relative">
-                        <div class="slider-arrow slider-arrow-2 carausel-6-columns-arrow" id="carausel-6-columns-arrows"></div>
-                        <div class="carausel-6-columns" id="carausel-6-columns">
-                            
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    </div>
-</section>
+
 @endsection
