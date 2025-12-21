cf910cba1c738811112c100c888bf51c10ab3ffe
Ritish <devteam@yazoeat.com.au>
2025-12-21 13:38:46 +0530

    Update cart.blade.php

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -9,26 +9,4 @@
         </div>
     </div>
 </div>
-<section class="mt-50 mb-50" id="cart-page">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="table-responsive" style="overflow:unset">
-                    <table class="table shopping-summery clean">
-                        <thead>
-                            <tr class="main-heading">
-                                <th scope="col">Image</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Quantity</th>
-                                <th scope="col">Subtotal</th>
-                                <th scope="col">Remove</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 @endsection
