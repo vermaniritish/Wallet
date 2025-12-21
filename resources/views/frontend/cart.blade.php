d8f3684d93c371f727cdaeb415619dc006f2a9a4
Ritish <devteam@yazoeat.com.au>
2025-12-21 13:37:19 +0530

    Update cart.blade.php

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -25,42 +25,7 @@
                                 <th scope="col">Remove</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr  v-if="cart && cart.length > 0" v-for="c in cart">
-                                <td class="image product-thumbnail">
-                                    <img class="border-radius-5" :src="getImagePath(c.image)">
-                                </td>
-                                <td class="product-des product-name">
-                                    <h5 class="product-name"><a :href="'/' + c.slug">@{{ c.title }}</a></h5>
-                                    <p v-if="c.non_exchange == 1 || c.non_exchange == '1'" class="font-xs" style="color: rgb(209, 0, 31);"> Made to order only. This is a Non-Exchangeable &amp; Non-Refundable product.</p>
-                                    <p class="font-xs">SKU: @{{c.sku_number}}<br> Size:  @{{c.size_title}}<br/> Color: @{{c.color}} </p>
-                                    
-                                    <div class="font-xs">
-                                        
-                                    </div>
-                                </td>
-                                <td class="price" data-title="Price"><span>£@{{c.price}}</span></td>
-                                <td class="text-center" data-title="Stock">
-                                    <div class="detail-qty border radius  m-auto">
-                                        <a href="javascript:;" v-on:click="decrement(c.id)" class="qty-down"><i class="fi-rs-angle-small-down"></i></a>
-                                        <span class="qty-val">@{{ c.quantity && c.quantity > 0 ? c.quantity : `` }}</span>
-                                        <a href="javascript:;" v-on:click="increment(c.id)" class="qty-up"><i class="fi-rs-angle-small-up"></i></a>
-                                    </div>
-                                </td>
-                                <td class="text-right" data-title="Cart">
-                                    <span>£@{{(c.quantity * c.price).toFixed(2)}}</span>
-                                </td>
-                                <td class="action" data-title="Remove"><a href="javascript:;" v-on:click="remove(c.id)" class="text-muted"><i class="fi-rs-trash"></i></a></td>
-                            </tr>
-                            <tr v-if="!cart || cart.length < 1">
-                                <td colspan="5" class="cart__table--body__list"><p class="text-center py-5">Your cart is empty. No product is availble.</p></td>
-                            </tr>
-                            <tr v-if="cart && cart.length > 0" >
-                                <td colspan="6" class="text-end">
-                                    <a href="#" v-on:click="clearCart" class="text-muted"> <i class="fi-rs-cross-small"></i> Clear Cart</a>
-                                </td>
-                            </tr>
-                        </tbody>
+                       
                     </table>
                 </div>
                 <div class="divider center_icon mt-50 mb-50"><i class="fi-rs-fingerprint"></i></div>
