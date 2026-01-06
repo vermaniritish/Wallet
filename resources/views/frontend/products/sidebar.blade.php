e2ed8b58f7f5819888b5af603bf131ca37f193c5
Ritish <devteam@yazoeat.com.au>
2026-01-06 22:56:03 +0530

    products sidebar

--- resources/views/frontend/products/sidebar.blade.php
+++ resources/views/frontend/products/sidebar.blade.php
@@ -1,3 +1,14 @@
+<?php
+use App\Models\Admin\Sliders;
+use App\Models\API\Products;
+$rightTopSlide = Sliders::where('status',1)->orderByRaw('RAND()')->where('type', 'right_top')->limit(1)->first();
+$rightBottomSlide = Sliders::where('status',1)->orderByRaw('RAND()')->where('type', 'right_bottom')->limit(1)->first();	
+$newProducts = Products::getListing($request, [
+    'products.status' => 1,
+    'products.website_visible' => 1,
+    '(products.id IN ('.implode(',', $cids).'))'
+], 5);
+?>
 <div class="widget-category mb-15">
     <h5 class="section-title style-1 mb-30 wow fadeIn animated">Category</h5>
     <ul class="categories">
@@ -60,47 +71,57 @@
     </div>
     
     <!-- Product sidebar Widget -->
+    @if($newProducts)
     <div class="sidebar-widget product-sidebar  mb-30 p-30 bg-grey border-radius-10">
         <div class="widget-header position-relative mb-20 pb-10">
             <h5 class="widget-title mb-10">New products</h5>
             <div class="bt-1 border-color-1"></div>
         </div>
+        
+        @foreach($newProducts as $product)
         <div class="single-post clearfix">
             <div class="image">
-                <img src="{{ url('frontend/assets/imgs/shop/thumbnail-3.jpg') }}" alt="#">
+                <a href="{{ url('/'.$product->slug) }}">
+                    @foreach($product->image as $k => $image)
+                        @if($k < 1)
+                        <img src="{{ $image && $image['small'] ? url($image['small']) : url('/frontend/assets/imgs/shop/product-3-1.jpg') }}" alt="product-img">
+                        @endif
+                    @endforeach
+                    @foreach($product->image as $k => $image)
+                        @if($k == 1)
+                        <img src="{{ $image && $image['small'] ? url($image['small']) : url('/frontend/assets/imgs/shop/product-3-1.jpg') }}" alt="product-img">
+                        @endif
+                    @endforeach
+                </a>
             </div>
             <div class="content pt-10">
-                <h5><a href="shop-product-detail.html">Chen Cardigan</a></h5>
-                <p class="price mb-0 mt-5">$99.50</p>
-                
+                <h5><a href="{{ url('/'.$product->slug) }}">{{$product['title']}}</a></h5>
+                <p class="price mb-0 mt-5">{{_currency($product['price'])}}</p>
+                <div class="product-rate">
+                    <div class="product-rating" style="width:90%"></div>
+                </div>
             </div>
         </div>
-        <div class="single-post clearfix">
-            <div class="image">
-                <img src="{{ url('frontend/assets/imgs/shop/thumbnail-3.jpg') }}" alt="#">
-            </div>
-            <div class="content pt-10">
-                <h6><a href="shop-product-detail.html">Chen Sweater</a></h6>
-                <p class="price mb-0 mt-5">$89.50</p>
-                
-            </div>
-        </div>
-        <div class="single-post clearfix">
-            <div class="image">
-                <img src="{{ url('frontend/assets/imgs/shop/thumbnail-3.jpg') }}" alt="#">
-            </div>
-            <div class="content pt-10">
-                <h6><a href="shop-product-detail.html">Colorful Jacket</a></h6>
-                <p class="price mb-0 mt-5">$25</p>
-                
-            </div>
-        </div>
+        @endforeach
     </div>
-    <div class="banner-img wow fadeIn mb-45 animated d-lg-block d-none">
-        <img src="{{ url('frontend/assets/imgs/banner/banner-11.jpg') }}" alt="">
+    @endif
+    @if($rightTopSlide)
+    <div class="banner-img banner-1 wow fadeIn  animated home-3 w-100">
+        <img class="border-radius-10 w-100" src="{{ url($rightTopSlide->image) }}" alt="">
         <div class="banner-text">
-            <span>Women Zone</span>
-            <h4>Save 17% on <br>Office Dress</h4>
-            <a href="category-products.php">Shop Now <i class="fi-rs-arrow-right"></i></a>
+            <span>{{$rightTopSlide->label}}</span>
+            <h4><?php echo nl2br($rightTopSlide->heading) ?></h4>
+            <a href="{{ $rightTopSlide->button_url }}">{{ $rightTopSlide->button_title }} <i class="fi-rs-arrow-right"></i></a>
         </div>
     </div>
+    @endif
+    @if($rightBottomSlide)
+    <div class="banner-img banner-2 wow fadeIn  animated mb-0 w-100">
+        <img class="border-radius-10 w-100" src="{{ url($rightBottomSlide->image) }}" alt="">
+        <div class="banner-text">
+            <span>{{$rightBottomSlide->label}}</span>
+            <h4><?php echo nl2br($rightBottomSlide->heading) ?></h4>
+            <a href="{{ $rightBottomSlide->button_url }}">{{ $rightBottomSlide->button_title }} <i class="fi-rs-arrow-right"></i></a>
+        </div>
+    </div>
+    @endif
--- resources/views/frontend/school/sidebar.blade.php
+++ resources/views/frontend/school/sidebar.blade.php
@@ -1,66 +1,81 @@
+<?php
+use App\Models\Admin\Sliders;
+use App\Models\API\Products;
+$rightTopSlide = Sliders::where('status',1)->orderByRaw('RAND()')->where('type', 'right_top')->limit(1)->first();
+$rightBottomSlide = Sliders::where('status',1)->orderByRaw('RAND()')->where('type', 'right_bottom')->limit(1)->first();	
+$newProducts = Products::getListing($request, [
+    'products.status' => 1,
+    'products.website_visible' => 1,
+    '(products.id IN ('.implode(',', $cids).'))'
+], 5);
+?>
 <div class="col-lg-3 primary-sidebar sticky-sidebar">
-                <div class="row">
-                    <div class="col-lg-12 col-mg-6"></div>
-                    <div class="col-lg-12 col-mg-6"></div>
-                </div>
-                <div class="widget-category mb-30">
-                    <h5 class="section-title style-1 mb-30 wow fadeIn animated">Explore Our Full Collection</h5>
-                    <ul class="categories">
-                        @foreach($categories as $c)
-                        <li><a href="{{ url('/'.$c->slug) }}">{{ $c->title }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                
-                <!-- Product sidebar Widget -->
-                <div class="sidebar-widget product-sidebar  mb-30 p-30 bg-grey border-radius-10">
-                    <div class="widget-header position-relative mb-20 pb-10">
-                        <h5 class="widget-title mb-10">New products</h5>
-                        <div class="bt-1 border-color-1"></div>
-                    </div>
-                    <div class="single-post clearfix">
-                        <div class="image">
-                            <img src="assets/imgs/shop/thumbnail-3.jpg" alt="#">
-                        </div>
-                        <div class="content pt-10">
-                            <h5><a href="shop-product-detail.html">Chen Cardigan</a></h5>
-                            <p class="price mb-0 mt-5">£99.50</p>
-                            <div class="product-rate">
-                                <div class="product-rating" style="width:90%"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="single-post clearfix">
-                        <div class="image">
-                            <img src="assets/imgs/shop/thumbnail-4.jpg" alt="#">
-                        </div>
-                        <div class="content pt-10">
-                            <h6><a href="shop-product-detail.html">Chen Sweater</a></h6>
-                            <p class="price mb-0 mt-5">£89.50</p>
-                            <div class="product-rate">
-                                <div class="product-rating" style="width:80%"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="single-post clearfix">
-                        <div class="image">
-                            <img src="assets/imgs/shop/thumbnail-5.jpg" alt="#">
-                        </div>
-                        <div class="content pt-10">
-                            <h6><a href="shop-product-detail.html">Colorful Jacket</a></h6>
-                            <p class="price mb-0 mt-5">£25</p>
-                            <div class="product-rate">
-                                <div class="product-rating" style="width:60%"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="banner-img wow fadeIn mb-45 animated d-lg-block d-none">
-                    <img src="assets/imgs/banner/banner-11.jpg" alt="">
-                    <div class="banner-text">
-                        <span>Women Zone</span>
-                        <h4>Save 17% on <br>Office Dress</h4>
-                        <a href="#">Shop Now <i class="fi-rs-arrow-right"></i></a>
-                    </div>
+    <div class="row">
+        <div class="col-lg-12 col-mg-6"></div>
+        <div class="col-lg-12 col-mg-6"></div>
+    </div>
+    <div class="widget-category mb-30">
+        <h5 class="section-title style-1 mb-30 wow fadeIn animated">Explore Our Full Collection</h5>
+        <ul class="categories">
+            @foreach($categories as $c)
+            <li><a href="{{ url('/'.$c->slug) }}">{{ $c->title }}</li>
+            @endforeach
+        </ul>
+    </div>
+    
+    <!-- Product sidebar Widget -->
+     @if($newProducts)
+    <div class="sidebar-widget product-sidebar  mb-30 p-30 bg-grey border-radius-10">
+        <div class="widget-header position-relative mb-20 pb-10">
+            <h5 class="widget-title mb-10">New products</h5>
+            <div class="bt-1 border-color-1"></div>
+        </div>
+        
+        @foreach($newProducts as $product)
+        <div class="single-post clearfix">
+            <div class="image">
+                <a href="{{ url('/'.$product->slug) }}">
+                    @foreach($product->image as $k => $image)
+                        @if($k < 1)
+                        <img src="{{ $image && $image['small'] ? url($image['small']) : url('/frontend/assets/imgs/shop/product-3-1.jpg') }}" alt="product-img">
+                        @endif
+                    @endforeach
+                    @foreach($product->image as $k => $image)
+                        @if($k == 1)
+                        <img src="{{ $image && $image['small'] ? url($image['small']) : url('/frontend/assets/imgs/shop/product-3-1.jpg') }}" alt="product-img">
+                        @endif
+                    @endforeach
+                </a>
+            </div>
+            <div class="content pt-10">
+                <h5><a href="{{ url('/'.$product->slug) }}">{{$product['title']}}</a></h5>
+                <p class="price mb-0 mt-5">{{_currency($product['price'])}}</p>
+                <div class="product-rate">
+                    <div class="product-rating" style="width:90%"></div>
                 </div>
             </div>
+        </div>
+        @endforeach
+    </div>
+    @endif
+    @if($rightTopSlide)
+    <div class="banner-img banner-1 wow fadeIn  animated home-3 w-100">
+        <img class="border-radius-10 w-100" src="{{ url($rightTopSlide->image) }}" alt="">
+        <div class="banner-text">
+            <span>{{$rightTopSlide->label}}</span>
+            <h4><?php echo nl2br($rightTopSlide->heading) ?></h4>
+            <a href="{{ $rightTopSlide->button_url }}">{{ $rightTopSlide->button_title }} <i class="fi-rs-arrow-right"></i></a>
+        </div>
+    </div>
+    @endif
+    @if($rightBottomSlide)
+    <div class="banner-img banner-2 wow fadeIn  animated mb-0 w-100">
+        <img class="border-radius-10 w-100" src="{{ url($rightBottomSlide->image) }}" alt="">
+        <div class="banner-text">
+            <span>{{$rightBottomSlide->label}}</span>
+            <h4><?php echo nl2br($rightBottomSlide->heading) ?></h4>
+            <a href="{{ $rightBottomSlide->button_url }}">{{ $rightBottomSlide->button_title }} <i class="fi-rs-arrow-right"></i></a>
+        </div>
+    </div>
+    @endif
+</div>
