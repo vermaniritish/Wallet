3b53d481b0c23121d0b2e0ef1107747fc27ade0f
Ritish <devteam@yazoeat.com.au>
2026-01-11 13:57:56 +0530

    Update main.js

--- public/frontend/assets/js/main.js
+++ public/frontend/assets/js/main.js
@@ -582,21 +582,26 @@
     $offCanvasNavSubMenu.slideUp();
 
     /*Category Sub Menu Toggle*/
-    $offCanvasNav.on('click', 'li a, li .menu-expand', function(e) {
-        var $this = $(this);
-        if ( ($this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/)) && ($this.attr('href') === '#' || $this.hasClass('menu-expand')) ) {
-            e.preventDefault();
-            if ($this.siblings('ul:visible').length){
-                $this.parent('li').removeClass('active');
-                $this.siblings('ul').slideUp();
-            } else {
-                $this.parent('li').addClass('active');
-                $this.closest('li').siblings('li').removeClass('active').find('li').removeClass('active');
-                $this.closest('li').siblings('li').find('ul:visible').slideUp();
-                $this.siblings('ul').slideDown();
-            }
-        }
+    $offCanvasNav.find('.menu-item-has-children .menu-expand, .menu-item-has-children > a').on('click', function() {
+        console.log('yesssss');
+        $(this).parent().find('.dropdown').toogleSlide();
     });
+
+    // $offCanvasNav.on('click', 'li a, li .menu-expand', function(e) {
+    //     var $this = $(this);
+    //     if ( ($this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/)) && ($this.attr('href') === '#' || $this.hasClass('menu-expand')) ) {
+    //         e.preventDefault();
+    //         if ($this.siblings('ul:visible').length){
+    //             $this.parent('li').removeClass('active');
+    //             $this.siblings('ul').slideUp();
+    //         } else {
+    //             $this.parent('li').addClass('active');
+    //             $this.closest('li').siblings('li').removeClass('active').find('li').removeClass('active');
+    //             $this.closest('li').siblings('li').find('ul:visible').slideUp();
+    //             $this.siblings('ul').slideDown();
+    //         }
+    //     }
+    // });
 
     /*--- language currency active ----*/
     $('.mobile-language-active').on('click', function(e) {
