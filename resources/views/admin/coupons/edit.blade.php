288d91abda548ff6697d542fa402fee0cbcf3c3c
Ritish <devteam@yazoeat.com.au>
2026-01-11 15:57:31 +0530

    Update edit.blade.php

--- resources/views/admin/coupons/edit.blade.php
+++ resources/views/admin/coupons/edit.blade.php
@@ -29,7 +29,7 @@
 					</div>
 				</div>
 				<div class="card-body">
-					<form method="post" action="<?php echo route('admin.coupons.add') ?>" class="form-validation">
+					<form method="post" action="<?php echo route('admin.coupons.edit', ['id' => $page->id]) ?>" class="form-validation">
 						<!--!! CSRF FIELD !!-->
 						{{ @csrf_field() }}
 						<h6 class="heading-small text-muted mb-4">Coupon information</h6>
