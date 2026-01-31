fa50f822df87652f07a7989d9cb036dca0526172
Ritish <devteam@yazoeat.com.au>
2026-01-31 18:06:02 +0530

    Update ProductsController.php

--- app/Http/Controllers/Admin/Products/ProductsController.php
+++ app/Http/Controllers/Admin/Products/ProductsController.php
@@ -102,7 +102,7 @@ class ProductsController extends AppController
     	}
 
 		$where['products.is_uniform'] = 0;
-		pr($where); die;
+		
     	$listing = Products::getListing($request, $where);
 
     	if($request->ajax())
