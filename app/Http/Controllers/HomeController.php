e57f2b1ac9147704beb18ace528796ab835a5697
Ritish <devteam@yazoeat.com.au>
2026-04-04 12:37:27 +0530

    fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -122,8 +122,6 @@ class HomeController extends BaseController
             ->where('product_id', $product->id)
 			->orderBy('sizes.sort_order', 'asc')
 			->get();
-
-			pr($product->sizes); die;
 			
 			$sizeTitles = $product->sizes
 				->pluck('size_title')
--- public/frontend/assets/js/product-listing.js
+++ public/frontend/assets/js/product-listing.js
@@ -117,6 +117,7 @@ if($('#product-page').length)
             },
             renderSizes() {
                 if(this.color) {
+                    console.log(`sizes1`, this.sizes.filter((i) => i.color_id == this.color ));
                     return this.sizes.filter((i) => i.color_id == this.color );
                 }
                 else {
